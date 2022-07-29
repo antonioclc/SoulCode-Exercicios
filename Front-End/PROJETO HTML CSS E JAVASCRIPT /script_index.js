b100d0b05a0a14f2d75914c4f0fcdaf841b080a6
Antônio Campos <aclcampos@live.com>
2022-07-29 14:32:13 -0300

    Projeto html, css e js. Grupo 1

---
+++ Front-End/PROJETO HTML CSS E JAVASCRIPT /script_index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+function mostrarCadastro() {
+  // eslint-disable-next-line no-alert
+  alert('seu cadastro foi realizado com sucesso, em breve entraremos em contato com você');
+}
