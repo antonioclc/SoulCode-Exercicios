d4e7a4d038ec0f60f8526ecc2eef059c5eaca648
Antônio Campos <aclcampos@live.com>
2022-08-01 10:05:12 -0300

    Projeto html, css e js. Grupo 1

--- Front-End/PROJETO HTML CSS E JAVASCRIPT /script_index.js
+++ Front-End/PROJETO HTML CSS E JAVASCRIPT /script_index.js
@@ -7,18 +7,23 @@ function mostrarCadastro() {
   const telefone = document.getElementById('telefone');
   const email = document.getElementById('email');
 
-  function verifyEmail(emailAVerificar) {
+  function verificarEmail(emailAVerificar) {
     return /\w+@\w+\.\w+/g.test(emailAVerificar);
   }
 
+  // VERIFICA NOME
   if (nome.value.length <= 3) {
     alert('Nome tem que ter mais de 3 caracteres');
+    // VERIFICA CPF
   } else if (cpf.value.length !== 11) {
     alert('CPF tem que ter 11 números');
+    // VERIFICA TELEFONE
   } else if (telefone.value.length <= 8) {
     alert('Telefone tem que ter mais de 8 caracteres');
-  } else if (!verifyEmail(email.value)) {
+    // VERIFICA EMAIL
+  } else if (!verificarEmail(email.value)) {
     alert('Email inválido!');
+    // SE TUDO TIVER CERTO, EXIBE A MENSAGEM DE CADASTRO E RESETA TODOS OS CAMPOS
   } else {
     alert('seu cadastro foi realizado com sucesso, em breve entraremos em contato com você');
     nome.value = '';
